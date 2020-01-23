c1240771c21e2bea52d001e5bf4af520c76056f9
Yelken Gonzales <yelkezin@gmail.com>
2020-01-23 14:37:00 -0300

    Primeiro commit

---
+++ src/br/com/oca/variables/Test.java
@@ -0,0 +1,14 @@
+package br.com.oca.variables;
+
+public class Test {
+    static int f = 10;
+
+    public static void main(String[] args) {
+        for (int i = 0;i < 20; i++) {
+            System.out.println(i);
+        }
+
+        int i = 15;
+        System.out.println(i);
+    }
+}
---
+++ src/br/com/oca/variables/Variables.java
@@ -0,0 +1,21 @@
+package br.com.oca.variables;
+
+public class Variables {
+
+    int i = 50;
+    static String name = "Navigator";
+
+    public void m1() { // method open
+        int x = 10; // local variable
+
+        if (x >= 10) { // opening
+            int y = 18; // Local Variable
+        } // close
+
+
+    } // method closed
+
+    int j = 14;
+
+
+}
